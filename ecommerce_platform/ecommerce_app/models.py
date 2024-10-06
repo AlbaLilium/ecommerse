8ce8ac52f85aab2c05bf5876ddf093ecba5eea9e
AlbaLilium <flos.alba.lilium@gmail.com>
2024-10-06 16:55:05 +0300

    feat: add User, ShoppingCart, Product, Seller models

--- ecommerce_platform/ecommerce_app/models.py
+++ ecommerce_platform/ecommerce_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.views.decorators.http import condition
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=120)
+
+    class Meta:
+        abstract = True
+
+class Seller(User):
+   website = models.URLField(default='')
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField()
+    cost = models.DecimalField()
+    sellers = models.ManyToManyField(Seller, related_name='product', on_delete=models.CASCADE)
+    available = models.BooleanField()
+
+    class Meta:
+        abstract = True
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(cost__gte=0) & models.Q(expensive_check=condition),
+                name="check_product_cost",
+            ),
+        ]
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ManyToManyField(Product, on_delete=models.CASCADE)
+
