23be5648a5c6fad2a5e68d2ec59ba5b41a67dd09
AlbaLilium <flos.alba.lilium@gmail.com>
2024-11-05 12:43:09 +0300

    fix: remove some bugs
    - Delete a constraint "delete on cascade"in ManyToManyRelations.
    - Add max lenght to charfield for choices in Payment model.
    - Delete constraint in product.
    - Style update.

--- backend/core/models.py
+++ backend/core/models.py
@@ -11,14 +11,14 @@ class User(models.Model):
 
 
 class Customer(User):
-    name = models.CharField(max_length=50, default='')
-    surname = models.CharField(max_length=30, default='')
+    name = models.CharField(max_length=50, default="")
+    surname = models.CharField(max_length=30, default="")
 
 
 class Seller(User):
     name = models.CharField(max_length=30)
-    description = models.TextField(max_length=300, default='')
-    website = models.URLField(default='')
+    description = models.TextField(max_length=300, default="")
+    website = models.URLField(default="")
 
 
 class Product(models.Model):
@@ -26,28 +26,20 @@ class Product(models.Model):
     description = models.TextField()
     image = models.ImageField()
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    sellers = models.ManyToManyField(Seller, related_name='product', on_delete=models.CASCADE)
+    sellers = models.ManyToManyField(Seller, related_name="product")
     available = models.BooleanField()
-
-    class Meta:
-        abstract = True
-        constraints = [
-            models.CheckConstraint(
-                condition=models.Q(cost__gte=0) & models.Q(expensive_check=condition),
-                name="check_product_cost",
-            ),
-        ]
 
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    shopping_cart = models.ForeignKey("ShoppingCart", on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     item_total = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 class ShoppingCart(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, on_delete=models.CASCADE, through=Order)
+    products = models.ManyToManyField(Product, through=Order)
     quantity = models.PositiveIntegerField(default=1)
     item_total = models.DecimalField(max_digits=10, decimal_places=2)
 
@@ -62,9 +54,9 @@ class Payment(models.Model):
         FAILED = 0, "Failed"
         SUCCESS = 1, "Success"
 
-    status = models.CharField(choices=PaymentStatus)
+    status = models.CharField(choices=PaymentStatus, max_length=7)
     order = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    payment_method = models.CharField(choices=PaymentMethod)
+    payment_method = models.CharField(choices=PaymentMethod, max_length=11)
     date = models.DateTimeField(auto_now_add=True)
